b722aa354017856ff3518863e508cc8be84080bb
munam <munam.ramay99@gmail.com>
2023-11-08 18:39:17 +0500

    headlessui created search box with functionality

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,15 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
-export { Hero, Navbar, Footer, CustomButton };
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+export {
+  Hero,
+  Navbar,
+  Footer,
+  CustomButton,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
