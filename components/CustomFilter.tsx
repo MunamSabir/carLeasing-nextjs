8dc66b37cf6fcb712d767a192659427f246ff268
munam <munam.ramay99@gmail.com>
2023-11-11 22:47:45 +0500

    done with custom Filter: we can search with gas type and year as well

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,10 +4,71 @@ import { useState, Fragment } from "react";
 import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
-  return <div>CustomFilter</div>;
+  const router = useRouter();
+
+  // e of type { type: string; value: string; } // need to adjust type here
+  const handleUpdateParams = (e: any) => {
+    const newPathName = updateSearchParams(title, e.value.toLocaleLowerCase());
+    router.push(newPathName);
+  };
+
+  return (
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+      >
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              alt="chevron up down"
+              height={20}
+              width={20}
+              className="ml-4 object-contain"
+            />
+          </Listbox.Button>
+          <Transition
+            // as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          ></Transition>
+          <Listbox.Options className="custom-filter__options">
+            {options.map((option) => (
+              <Listbox.Option
+                key={option.title}
+                value={option}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 px-4 ${
+                    active ? "bg-primary-blue text-white" : "text-gray-900"
+                  }`
+                }
+              >
+                {({ selected }) => (
+                  <span
+                    className={`block truncate ${
+                      selected ? "font-medium" : "font-normal"
+                    }`}
+                  >
+                    {option.title}
+                  </span>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </div>
+      </Listbox>
+    </div>
+  );
 };
 
 export default CustomFilter;
